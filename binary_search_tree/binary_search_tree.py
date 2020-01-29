3758e667dd81ffbe98072013e5cf3fd717becdfb
Daniel Wallen <daniel.wallen1987@gmail.com>
2020-01-29 17:00:35 -0500

    You know the drill. Added comments, deleted one line at a time, and repeated until the steps were crystal clear (easy to follow, impossible to misinterpret).

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -9,49 +9,76 @@ class BinarySearchTree:
 
     # Insert the given value into the tree
     def insert(self, value):
+        # If input value is less than root/current value (self.value)...
         if value < self.value:
+            # Assuming there's nothing on the left side (self.left)...
             if self.left is None:
+                # Assign a new BST(value) to self.left
                 self.left = BinarySearchTree(value)
             else:
+                # Otherwise (not empty), call this method on self.left -- going through the logic again = how value arg lands in the correct order
                 self.left.insert(value)
+        # If input value is greater than root/current value (self.value)...
         else:
+            # Assuming there's nothing on the right side (self.right)...
             if self.right is None:
+                # Assign a new BST(value) to self.right
                 self.right = BinarySearchTree(value)
             else:
+                # Otherwise (not empty), call this method again on self.right -- same reasoning as ^^^
                 self.right.insert(value)
 
-    # Return True if the tree contains the value
-    # False if it does not
+    # Return True if the tree contains the value or False if it does not
     def contains(self, target):
+        # If target (input value) *equals* root/current value (self.value)...
         if target == self.value:
+            # Return True
             return True
+        # If target is less than root/current value...
         if target < self.value:
+            # If there's nothing on the left...
             if not self.left:
+                # Return False (input doesn't exist)
                 return False
             else:
+                # Otherwise (stuff on left), return + call this method again on self.left -- gotta repeat same logic until target == value or False
                 return self.left.contains(target)
+        # If target is greater than root/current value...
         if target > self.value:
+            # If there's nothing on the right...
             if not self.right:
+                # Return False (input doesn't exist)
                 return False
             else:
+                # Otherwise (stuff on right), return + call this method again on self.right -- ^^^ ditto
                 return self.right.contains(target)
+        # No else necessary here because it's impossible for an input to not eventually hit one of the above functional criteria
 
     # Return the maximum value found in the tree
     def get_max(self):
+        # If there's anything on the right (where bigger elements live)...
         if self.right:
+            # Return + call this method on self.right until the max is located
             return self.right.get_max()
         else:
+            # Otherwise, return root/current value (self.value) because there are no larger numbers
             return self.value
 
     # Call the function `cb` on the value of each node
     # You may use a recursive or iterative approach
     def for_each(self, cb):
+        # Call cb and pass in root/current value (self.value)
         cb(self.value)
+        # If there are elements on the right...
         if self.right:
+            # Call *this* method on all of them and pass the callback function in as an argument
             self.right.for_each(cb)
+        # If there are elements on the left...
         if self.left:
+            # Call *this* method on all of them and pass cb in as an argument
             self.left.for_each(cb)
         else:
+            # Otherwise, the work is done, so you can simply return
             return
 
     # DAY 2 Project -----------------------
